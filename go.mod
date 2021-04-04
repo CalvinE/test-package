7ae3b9d4195d71171f104b69df1c2e1f7bd3dde5
Calvin Echols <calvin.echols@gmail.com>
2021-04-04 13:48:34 -0400

    real version 2.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/calvine/test-package
+module github.com/calvine/test-package/v2
 
 go 1.12
--- sample.go
+++ sample.go
@@ -3,6 +3,6 @@ package sample
 import "fmt"
 
 func MySampleFunc() {
-	fmt.Println("Version 2.1.3 of this Function")
+	fmt.Println("Version 2.0.0 of this Function")
 	fmt.Println("Hello World")
 }
